de3aaf93581753874539609a74ea24f1e40b93d8
pdlmn <djeinwings@gmail.com>
2022-05-02 22:53:01 +0200

    feat: add queue solution

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,22 +14,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.head
+  }
+
+  enqueue(value) {
+    const node = new ListNode(value)
+    if (!this.head) {
+      this.head = node
+    } else {
+      let curr = this.head
+      while (curr.next) {
+        curr = curr.next
+      }
+      curr.next = node
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.head) return null
+
+    const deleted = this.head
+    if (deleted.next) {
+      this.head = deleted.next
+    } else {
+      this.head = null
+    }
+    return deleted.value
   }
 }
+
+const queue = new Queue()
+queue.enqueue(5)
+queue.enqueue(6)
+queue.enqueue(7)
+console.log(queue.dequeue())
+console.log(queue.dequeue())
+console.log(queue.dequeue())
 
 module.exports = {
   Queue
